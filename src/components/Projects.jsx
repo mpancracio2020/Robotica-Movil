97f053ebf0e987dd3856c627c7f4fefa7701a667
Marvin <90764466+mpancracio2020@users.noreply.github.com>
2023-10-05 19:31:17 +0200

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -74,7 +74,7 @@ export default function Projects() {
               <div class="panel">
               <div class="panel-heading">
                 <h4 class="panel-title">
-                  <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">Collapsible Group 1</a>
+                  <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">Basic Vacuum Cleaner</a>
                 </h4>
                </div>
               <div id="collapse1" class="panel-collapse collapse in">
