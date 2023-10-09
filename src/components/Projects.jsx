769c94969c3e442b79fe89b23d4bddd4a8739003
Marvin <90764466+mpancracio2020@users.noreply.github.com>
2023-10-09 14:01:00 +0200

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Accordion from 'src/Accordion';
+import Accordion from '../pages/Accordion';
 import { useAppContext } from "../appContext";
 import { useSelector } from "react-redux";
 import {
