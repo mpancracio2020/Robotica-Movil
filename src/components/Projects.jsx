3815622579b40f19830af7e71de0b99a421094f7
Marvin <90764466+mpancracio2020@users.noreply.github.com>
2023-11-06 16:25:59 +0100

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -444,9 +444,9 @@ export default function Projects() {
                           
                           </pre>
                           </code>
-                Once we have the two components, it is time to calculate the resulting Force from which we will obtain linear and angular velocity {"\n"}
+                Once we have the two components, it is time to calculate the resulting Force from which we will obtain linear and angular velocity {"\n"}{"\n"}
 
-                And with that we would have the practice
+                And with that we would have the practice {"\n"}{"\n"}
 
 
                 Here is an example video: {"\n"} {"\n"}
