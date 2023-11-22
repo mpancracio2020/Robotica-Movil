dd3033aaf04c4296678f60c1cd4a75a675d53a46
Marvin <90764466+mpancracio2020@users.noreply.github.com>
2023-11-22 18:55:21 +0100

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -486,8 +486,8 @@ export default function Projects() {
                     <center><iframe width="560" height="315" src="https://www.youtube.com/embed/DZqT9qP41wE?si=FvD6IY6zgynJ4Y_z" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> </center>
                     
                     
-              </pre></p>
-             </p>
+              </p>
+            
 
 
 
